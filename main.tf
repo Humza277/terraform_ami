ca2e94c07953794dbcd79fcda7ab30d54d575e9f
hmalak <hmalak@spartaglobal.com>
2020-08-20 18:05:33 +0100

    Terraform creates images according to spec - DOD done

--- main.tf
+++ main.tf
@@ -1,15 +1,48 @@
 # Launch a ami on aws
-# waht do we want to do and where do we want to create instance
-# terraform syntax - simmilar to JSON 
+# what do we want to do and where do we want to create instance
+# terraform syntax - similar to JSON
 provider "aws" {
 #which region do we have the ami available in
   region = "eu-west-1"
 }
 # create from ami image
-resource "aws_instance" "app_instance" {ami = "ami-01561b0b028f2d8de"
-#what type of instance we want to create
-          instance_type = "t2.micro"
-# do we want public ip 
-	  associate_public_ip_address = true
-          tags {Name = "eng67.humza.terraform.web}
+resource "aws_instance" "app_instance" {
+  ami = "ami-01561b0b028f2d8de"
+
+  #what type of instance we want to create
+  instance_type = "t2.micro"
+  # do we want public ip
+  associate_public_ip_address = true
+  vpc_security_group_ids = ["${aws_security_group.ami_sec_group.id}"]
+  tags = {
+    Name = "eng67.humza.terraform.web"
+  }
+}
+resource "aws_subnet" "public_ip" {
+
+  cidr_block = "172.31.100.0/24"
+  vpc_id = var.vpc_id
+}
+
+resource "aws_security_group" "ami_sec_group" {
+  name = "ami_sec_group"
+  description = "allow traffic in"
+  vpc_id = var.vpc_id
+
+  ingress {
+    description = "allow inbound traffic"
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+    cidr_blocks = ["172.31.0.0/16"]
+  }
+  egress {
+    from_port = 0
+    protocol = -1
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "ami.humza"
+  }
 }
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "vpc_id" {
+  type = string
+  default = "vpc-07e47e9d90d2076da"
+}
+
+variable "name" {
+  type = string
+  default = "Eng67.humza.terraform.app"
+}
+
+variable "app_ami_id" {
+  type = string
+  default = "subnet-05f8bdd912bcffab9"
+}
+
